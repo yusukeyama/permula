3faed60648b6294ca6eb288517f3bbad3a922b5d
yamanishiyusuke <nextcentury2000@gmail.com>
2019-11-20 23:44:02 +0900

    [ADD]test code

---
+++ cmd/permula/permula_test.go
@@ -0,0 +1,18 @@
+package permula
+
+import (
+	"testing"
+)
+
+func TestScanToSlicebyInt(t *testing.T) {
+
+}
+
+func TestCombination(t *testing.T) {
+	//normal:3
+	 Combination(3, 2)
+}
+
+func TestPermutation(t *testing.T) {
+
+}
