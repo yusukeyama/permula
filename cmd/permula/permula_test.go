78fe397d3182416f740958d0c9100b3dbd2b5c6c
yamanishiyusuke <nextcentury2000@gmail.com>
2019-11-21 23:23:16 +0900

    [UP]test

--- cmd/permula/permula_test.go
+++ cmd/permula/permula_test.go
@@ -2,6 +2,7 @@ package permula
 
 import (
 	"testing"
+	"github.com/stretchr/testify/assert"
 )
 
 func TestScanToSlicebyInt(t *testing.T) {
@@ -10,7 +11,8 @@ func TestScanToSlicebyInt(t *testing.T) {
 
 func TestCombination(t *testing.T) {
 	//normal:3
-	 Combination(3, 2)
+	res := Combination(3, 2)
+	assert.Equal(t, res, 3)
 }
 
 func TestPermutation(t *testing.T) {
